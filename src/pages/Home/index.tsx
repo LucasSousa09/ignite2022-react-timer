7caa8c86e0ce1b103cc6b36f03ab03634f0fe941
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-11-28 11:31:31 -0300

    change the page title when the countdown reduces

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -94,6 +94,12 @@ export function Home() {
   const task = watch('task')
   const isSubmitDisabled = !task
 
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
+
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
